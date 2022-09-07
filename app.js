28c210000e9a24ebd2aeb3a82bca6a5914ad3cc8
swalheen26 <swaleheensid786@gmail.com>
2022-09-07 14:08:47 +0530

    app

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://swalheen26:Sheikh123@cluster0.wyoecxw.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to DB');
+}).catch((error) => {
+    console.log(error);
+});
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.use(require('./routes/index'));
+app.use(require('./routes/todo'));
+app.use(require('./routes/register'));
+app.use(require('./routes/auth'));
+
+const PORT = process.env.PORT||3000;
+app.listen(PORT, () => {
+    console.log(`sever listening on port: ${PORT}`);
+});
+
